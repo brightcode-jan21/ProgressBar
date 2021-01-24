6123dea71272071c75f8f2a3a4f6856372406d1c
Robert Chen <robghchen@gmail.com>
2021-01-24 17:35:11 -0500

    style(learningModule): revert prettier changes

--- src/components/learningModule/LearningModule.js
+++ src/components/learningModule/LearningModule.js
@@ -1,72 +1,67 @@
-import React from "react";
-import SelectionBox from "../selectionBox/SelectionBox";
-import Button from "../button/Button";
-import Intro from "../intro/Intro";
+import React from 'react';
+import SelectionBox from '../selectionBox/SelectionBox';
+import Button from '../button/Button';
+import Intro from '../intro/Intro';
 
-import "./Styles.scss";
+import './Styles.scss';
 
-const LearningModule = ({ setGameStatus, gameStatus }) => {
-    const [currentQuestionId, setCurrentQuestionId] = React.useState(0);
-    const [quizData, setQuizData] = React.useState({});
-    const [isComplete, setIsComplete] = React.useState(false);
-    const [isAnimateEnd, setIsAnimateEnd] = React.useState(false);
+const LearningModule = ({setGameStatus, gameStatus}) => {
+  const [currentQuestionId, setCurrentQuestionId] = React.useState(0);
+  const [quizData, setQuizData] = React.useState({});
+  const [isComplete, setIsComplete] = React.useState(false);
+  const [isAnimateEnd, setIsAnimateEnd] = React.useState(false);
+  
+  let currentQuestion = quizData.questionArr ? quizData.questionArr[currentQuestionId]: {};
+  
+  React.useEffect(()=>{
+    getQuizData();
+  },[]);
 
-    let currentQuestion = quizData.questionArr
-        ? quizData.questionArr[currentQuestionId]
-        : {};
+  React.useEffect(()=>{
+    console.log(gameStatus);
+  },[gameStatus]);
 
-    React.useEffect(() => {
-        getQuizData();
-    }, []);
 
-    React.useEffect(() => {
-        console.log(gameStatus);
-    }, [gameStatus]);
+  const getQuizData=()=>{
+    fetch("http://localhost:8080/problems")
+      .then((res)=>{
+        return res.json();
+      }).then((data)=>{
+        setQuizData(data);
+      }).catch((err)=>{
+        console.log(err);
+      });
+  }
 
-    const getQuizData = () => {
-        fetch("http://localhost:8080/problems")
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setQuizData(data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  const handleSubmit=()=> {
+    if(currentQuestionId < quizData.totalQuestions-1){
+      console.log(currentQuestionId)
+      setCurrentQuestionId(currentQuestionId+1);
+    } else if (!isComplete) {
+      setIsComplete(true);
+    } else {
+      setCurrentQuestionId(0);
+      setIsComplete(false);
+      setGameStatus('new');
+	}
+	setIsAnimateEnd(false);
+  }
+  let possibleAnswers = [];
+  if(currentQuestion.possibleAnswers){
+    possibleAnswers = currentQuestion.possibleAnswers.map((answer, index) => {
+      return <SelectionBox id={index} key={index} answer={answer} />
+    })
+  }
 
-    const handleSubmit = () => {
-        if (currentQuestionId < quizData.totalQuestions - 1) {
-            setCurrentQuestionId(currentQuestionId + 1);
-        } else if (!isComplete) {
-            setIsComplete(true);
-        } else {
-            setCurrentQuestionId(0);
-            setIsComplete(false);
-            setGameStatus("new");
-        }
-        setIsAnimateEnd(false);
-    };
-    let possibleAnswers = [];
-    if (currentQuestion.possibleAnswers) {
-        possibleAnswers = currentQuestion.possibleAnswers.map(
-            (answer, index) => {
-                return <SelectionBox id={index} key={index} answer={answer} />;
-            }
-        );
-    }
-
-    const progress = isComplete
-        ? 100
-        : ((currentQuestionId + 1) / (quizData.totalQuestions + 1)) * 100 || 0;
-    const prevProgress =
-        progress - (1 / (quizData.totalQuestions + 1)) * 100 || 0;
-
-    return (
-        <div className="learningModule">
-            <div className="learningModule__progressBar">
-                <div className="learningModule__progressBar--background" />
+  const progress = isComplete
+  ? 100
+  : ((currentQuestionId + 1) / (quizData.totalQuestions + 1)) * 100 || 0;
+const prevProgress =
+  progress - (1 / (quizData.totalQuestions + 1)) * 100 || 0;
+  return (
+    <div className="learningModule">
+		<div className="learningModule__progressBar">
+			<div className="learningModule__progressBar--background" />
                 <div className="learningModule__progressBar--ends">
                     <span />
                     <span />
@@ -87,40 +82,32 @@ const LearningModule = ({ setGameStatus, gameStatus }) => {
                     }}
                 />
             </div>
-            {currentQuestion.title && !isComplete && (
-                <>
-                    <div className="learningModule__header">
-                        <div className="learningModule__title">
-                            {currentQuestion.title}
-                        </div>
-                        <div className="learningModule__subHeader">
-                            {currentQuestion.additionalInfo}
-                        </div>
-                    </div>
+      { currentQuestion.title && !isComplete &&
+        <>
+          <div className="learningModule__header">
+            <div className="learningModule__title">
+              { currentQuestion.title }
+            </div>
+            <div className="learningModule__subHeader">
+              { currentQuestion.additionalInfo }
+            </div>
+          </div>
 
-                    <div className="learningModule__answerArea">
-                        <div className="learningModule__selections">
-                            {possibleAnswers}
-                        </div>
-                        <div className="learningModule__submitButtonContainer">
-                            <Button
-                                label="Submit"
-                                inactive
-                                handleSubmit={handleSubmit}
-                            />
-                        </div>
-                    </div>
-                </>
-            )}
-            {isComplete && (
-                <Intro
-                    message="Congratulations. You've completed this level!"
-                    buttonLabel="Play again"
-                    buttonClick={handleSubmit}
-                />
-            )}
-        </div>
-    );
-};
+          <div className="learningModule__answerArea">
+            <div className="learningModule__selections">
+              { possibleAnswers }
+            </div>
+            <div className="learningModule__submitButtonContainer">
+              <Button label="Submit" inactive handleSubmit={ handleSubmit } />
+            </div>
+          </div>
+        </>
+      }
+      {isComplete &&
+        <Intro message="Congratulations. You've completed this level!" buttonLabel="Play again"  buttonClick={handleSubmit} />
+      }
+    </div>
+  )
+}
 
 export default LearningModule;
